9872b50a2ce8d76c940705b8a56faa301dff7b9f
david-bol12 <davidboland1001@gmail.com>
2025-11-08 21:46:41 +0000

    Config

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ javafx {
 }
 
 dependencies {
+    implementation("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.prisongame.ui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     exports org.prisongame.ui;
     opens org.prisongame.ui to javafx.fxml;
